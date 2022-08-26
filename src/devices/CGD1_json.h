9ae5ae9d97beef59de4983a6bd7ef95181dcf225
adamrbell <adamrbell@gmail.com>
2022-08-26 20:59:29 +0100

    Update CGD1_json.h
    Added servicedata condition for newer firmware

--- src/devices/CGD1_json.h
+++ src/devices/CGD1_json.h
@@ -7,7 +7,7 @@ const char* _CGD1_json = "{\"brand\":\"Cleargrass\",\"model\":\"Alarm Clock\",\"
    "brand":"Cleargrass",
    "model":"Alarm Clock",
    "model_id":"CGD1",
-   "condition":["servicedata", "index", 0, "080caf", "|", "servicedata", "index", 0, "080c09", "|", "servicedata", "index", 0, "080cd0"],
+   "condition":["servicedata", "index", 0, "080caf", "|", "servicedata", "index", 0, "080c09", "|", "servicedata", "index", 0, "080cd0", "|", "servicedata", "index", 0, "880cd0"],
    "properties":{
       "tempc":{
          "decoder":["value_from_hex_data", "servicedata", 20, 4, true],
